b05adcd83e5e445b2ca218673c8774f1d258661f
Jordan Kline <jordan.kline@gmail.com>
2018-02-06 23:39:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,11 +3,11 @@ require 'sinatra/base'
 class App < Sinatra::Base
   get '/newteam' do
 
-    erb ':newteam'
+    erb :newteam
   end
 
   post '/team' do
 
-    erb '/team'
+    erb :team
   end
 end
